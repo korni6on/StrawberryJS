d31f312441c5112d1072acee224839e5e157fc34
ikostov <ivelin.kostov88@abv.bg>
2016-01-05 00:16:53 +0200

    Add StrawberryJS

---
+++ src/StrawberryJS.js
@@ -0,0 +1,63 @@
+(function () {
+
+    var S = function (params) {
+        return new Strawberry(params);
+    };
+
+    var Strawberry = function (params) {
+        var selector = document.querySelectorAll(params);
+        this.length = selector.length;
+        var len = this.length;
+        while (len--) {
+            this[len] = selector[len]
+        }
+
+        return this;
+    };
+
+    S.fn = Strawberry.prototype = {
+        about: {
+            version: '0.0.1',
+            homeurl: 'https://github.com/korni6on/StrawberryJS'
+        },
+        show: function () {
+            var len = this.length;
+            while (len--) {
+                this[len].style.display = 'block';
+                this[len].dispatchEvent(this.events.show);
+            }
+
+            return this;
+        },
+        hide: function () {
+            var len = this.length;
+            while (len--) {
+                this[len].style.display = 'none';
+            }
+
+            return this;
+        },
+        text: function (join) {
+            var len = this.length,
+                    text = [];
+            while (len--) {
+                text[len] = this[len].textContent;
+            }
+
+            if (join === true) {
+                return text.join('');
+            }
+            else if (typeof join === 'string') {
+                return text.join(join);
+            }
+            else {
+                return text;
+            }
+        }
+    };
+
+    // Assign our Q object to global window object.
+    if (!window.S) {
+        window.S = S;
+    }
+})();
